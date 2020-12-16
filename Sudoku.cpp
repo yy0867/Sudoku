9fc8e4dbb309ecd4736a26d02ec69542c4c06ceb
yy0867 <yy0867@naver.com>
2020-12-16 01:25:04 -0800

    modify square error v2

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -63,7 +63,7 @@ Sudoku::Sudoku(int level) {
                                : sudoku[i][j] = sudoku[i - 3][j] + 1;
 
     // randomize basic sudoku
-    //randomize_sudoku();
+    randomize_sudoku();
     make_blank(level);
 }
 
